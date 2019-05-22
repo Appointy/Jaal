be2525c14fb06b34e8ffb728259aedd54679bdef
Anuj Gupta <anuj.g@appointy.com>
2019-05-22 18:12:25 +0530

    Union testcases and Http Handler test cases added

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/golang/protobuf v1.3.1
 	github.com/graphql-go/graphql v0.7.8
+	github.com/kylelemons/godebug v1.1.0
 	github.com/srikrsna/sqlx v0.0.0-20180828084051-e347d2855cc9
 	github.com/stretchr/testify v1.3.0
 	go.appointy.com/idutil v0.0.0-20190516071107-282c24d3361d
---
+++ http_test.go
@@ -0,0 +1,115 @@
+package jaal_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/kylelemons/godebug/pretty"
+
+	"go.appointy.com/appointy/jaal"
+	"go.appointy.com/appointy/jaal/schemabuilder"
+)
+
+func testHTTPRequest(req *http.Request) *httptest.ResponseRecorder {
+	schema := schemabuilder.NewSchema()
+
+	query := schema.Query()
+	query.FieldFunc("mirror", func(args struct{ Value int64 }) int64 {
+		return args.Value * -1
+	})
+
+	builtSchema := schema.MustBuild()
+
+	rr := httptest.NewRecorder()
+	handler := jaal.HTTPHandler(builtSchema)
+
+	handler.ServeHTTP(rr, req)
+	return rr
+}
+
+func TestHTTPMustPost(t *testing.T) {
+	req, err := http.NewRequest("GET", "/graphql", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := testHTTPRequest(req)
+
+	if rr.Code != 200 {
+		t.Errorf("expected 200, but received %d", rr.Code)
+	}
+
+	if diff := pretty.Compare(rr.Body.String(), "{\"data\":null,\"errors\":[\"request must be a POST\"]}"); diff != "" {
+		t.Errorf("expected response to match, but received %s", diff)
+	}
+}
+
+func TestHTTPParseQuery(t *testing.T) {
+	req, err := http.NewRequest("POST", "/graphql", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := testHTTPRequest(req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected 200, but received %d", rr.Code)
+	}
+
+	if diff := pretty.Compare(rr.Body.String(), "{\"data\":null,\"errors\":[\"request must include a query\"]}"); diff != "" {
+		t.Errorf("expected response to match, but received %s", diff)
+	}
+}
+
+func TestHTTPMustHaveQuery(t *testing.T) {
+	req, err := http.NewRequest("POST", "/graphql", strings.NewReader(`{"query":""}`))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := testHTTPRequest(req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected 200, but received %d", rr.Code)
+	}
+
+	if diff := pretty.Compare(rr.Body.String(), "{\"data\":null,\"errors\":[\"must have a single query\"]}"); diff != "" {
+		t.Errorf("expected response to match, but received %s", diff)
+	}
+}
+
+func TestHTTPSuccess(t *testing.T) {
+	req, err := http.NewRequest("POST", "/graphql", strings.NewReader(`{"query": "query TestQuery($value: int64) { mirror(value: $value) }", "variables": { "value": 1 }}`))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := testHTTPRequest(req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected 200, but received %d", rr.Code)
+	}
+
+	if diff := pretty.Compare(rr.Body.String(), "{\"data\":{\"mirror\":-1},\"errors\":null}"); diff != "" {
+		t.Errorf("expected response to match, but received %s", diff)
+	}
+}
+
+func TestHTTPContentType(t *testing.T) {
+	req, err := http.NewRequest("POST", "/graphql", strings.NewReader(`{"query": "query TestQuery($value: int64) { mirror(value: $value) }", "variables": { "value": 1 }}`))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := testHTTPRequest(req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected 200, but received %d", rr.Code)
+	}
+
+	if diff := pretty.Compare(rr.HeaderMap.Get("Content-Type"), "application/json"); diff != "" {
+		t.Errorf("expected response to match, but received %s", diff)
+	}
+}
