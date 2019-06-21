16dddae4c022b3368d8459010a76b97b6d155a7b
Anuj Gupta <anuj.g@appointy.com>
2019-06-21 21:00:53 +0530

    gtypes updated

--- gtypes/global.go
+++ gtypes/global.go
@@ -2,13 +2,8 @@ package gtypes
 
 import (
 	"context"
-	"encoding/base64"
-	"encoding/json"
-	"errors"
 	"reflect"
-	"time"
 
-	"github.com/golang/protobuf/ptypes/duration"
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/iancoleman/strcase"
 	"go.appointy.com/jaal/schemabuilder"
@@ -19,82 +14,15 @@ import (
 var Schema = schemabuilder.NewSchema()
 
 func init() {
-	RegisterWellKnownTypes()
+	RegisterEmpty()
 	RegisterInputFieldMask()
 	RegisterPayloadFieldMask()
-}
-
-//RegisterWellKnownTypes registers the commonly used scalars
-func RegisterWellKnownTypes() {
-	RegisterDuration()
-	// RegisterTimestamp()
-	RegisterEmpty()
-	// RegisterStringStringMap()
-}
-
-// RegisterStringStringMap registers the map[string]string as a scalar
-func RegisterStringStringMap() {
-	typ := reflect.TypeOf(map[string]string{})
-	schemabuilder.RegisterScalar(typ, "Metadata", func(value interface{}, target reflect.Value) error {
-		v, ok := value.(string)
-		if !ok {
-			return errors.New("invalid type expected a string")
-		}
-
-		decodedValue, err := base64.StdEncoding.DecodeString(v)
-		if err != nil {
-			return err
-		}
-
-		data := make(map[string]string, 10)
-		if err := json.Unmarshal(decodedValue, &data); err != nil {
-			return err
-		}
-
-		target.Set(reflect.ValueOf(data))
-		return nil
-	})
 }
 
 // RegisterEmpty registers empty as an scalar type
 func RegisterEmpty() {
 	typ := reflect.TypeOf(empty.Empty{})
 	schemabuilder.RegisterScalar(typ, "Empty", func(value interface{}, target reflect.Value) error {
-		return nil
-	})
-}
-
-// RegisterDuration registers duration as an scalar type
-func RegisterDuration() {
-	typ := reflect.TypeOf(duration.Duration{})
-	schemabuilder.RegisterScalar(typ, "Duration", func(value interface{}, target reflect.Value) error {
-		v, ok := value.(float64)
-		if !ok {
-			return errors.New("invalid type expected a string")
-		}
-
-		target.Field(0).SetInt(int64(v))
-		target.Field(1).SetInt(0)
-		return nil
-	})
-}
-
-// RegisterTimestamp registers timestamp as a scalar type
-func RegisterTimestamp() {
-	typ := reflect.TypeOf(schemabuilder.Timestamp{})
-	schemabuilder.RegisterScalar(typ, "Timestamp", func(value interface{}, target reflect.Value) error {
-		v, ok := value.(string)
-		if !ok {
-			return errors.New("invalid type expected a string")
-		}
-
-		t, err := time.Parse(time.RFC3339, v)
-		if err != nil {
-			return err
-		}
-
-		target.Field(0).SetInt(int64(t.Second()))
-		target.Field(1).SetInt(int64(t.Nanosecond()))
 		return nil
 	})
 }
